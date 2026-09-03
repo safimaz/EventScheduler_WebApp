cb58dcdad55ede5e55e7fa0f34681fd0be311283
safimaz <mohammadsafimaz25@gmail.com>
2026-09-03 23:15:48 +0530

    add: UserService in service

---
+++ src/main/java/com/event/scheduler/service/UserService.java
@@ -0,0 +1,22 @@
+package com.event.scheduler.service;
+
+import java.util.List;
+
+import com.event.scheduler.model.User;
+
+public interface UserService {
+
+    boolean addUser(User user);
+
+    User getUserById(int userId);
+
+    User getUserByEmail(String email);
+
+    List<User> getAllUsers();
+
+    boolean updateUser(User user);
+
+    boolean deleteUser(int userId);
+
+    boolean validateLogin(String email, String password);
+}
